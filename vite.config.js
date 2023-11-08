25c1a29a5974181a688ffe419e85c12500589ced
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2023-11-07 23:04:02 -0300

    ✨: add post generator

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    watch: {
-      usePolling: true, 
-      interval: 1000,
-      binaryDirs: ["node_modules", "public"],
-  },
-});
+})
